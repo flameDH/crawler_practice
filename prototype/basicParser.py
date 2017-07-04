d45f1de5ec84a34614639f15b2c035cf02c37625
flameDH <TexasflameDH@gmail.com>
2017-07-04 22:10:10 +0800

    Add files via upload

---
+++ prototype/basicParser.py
@@ -0,0 +1,28 @@
+from pyquery import PyQuery as pq
+from urllib.request import Request,urlopen
+import os,codecs
+import sys
+
+class basicParse:
+
+	headers={'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
+	data = ''
+	def __init__(self,url):
+		self.url=url;
+
+	def parser(self):
+		req = Request(url=self.url,headers=self.headers)
+		raw = urlopen(req).read()
+		self.data =pq(raw)
+	def getUrl(self):
+		urlStack = self.data('a').attr('href');
+		print (urlStack)
+		#raise urlStack		
+def main():
+	test=basicParse('https://www.ptt.cc/bbs/Shadowverse/M.1497623782.A.DE3.html')
+	test.parser()
+	
+	test.getUrl()
+
+if __name__=='__main__':
+	main()	    		
